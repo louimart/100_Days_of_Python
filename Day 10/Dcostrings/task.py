8af90a1493369ccbf13cc598200608e46322d15b
Louis Martin <louimart@gmail.com>
2024-11-28 10:33:54 -0600

    how to add Docstrings to functions using """..."""

--- Day 10/Dcostrings/task.py
+++ Day 10/Dcostrings/task.py
@@ -1,4 +1,5 @@
 def format_name(f_name, l_name):
+    """Takes a first name and last name and returns a formated full name in Title Case"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
