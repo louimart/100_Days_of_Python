d01ab883e65f6af6d34d5baf51105ebbb8647e20
Louis Martin <louimart@gmail.com>
2024-11-28 10:11:50 -0600

    coding challenge to create a function that checks if a given input year is a leap year. Uses series of if statements checking if divisible with % modulo and multiple returns for desired boolean output.

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -7,3 +7,25 @@ def format_name(f_name, l_name):
 
 
 print(format_name(input("What is you first name?"), input("What is your last name?")))
+
+# Challenge to create a function that checks if a given input year is a leap year
+def is_leap_year(year):
+    # check if year is divisible by 4
+    if year % 4 == 0:
+        # check if year is not divisible by 100
+        if year % 100 != 0:
+            return True
+        # if year is divisible by 100 and also divisible by 400
+        elif year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+
+print(f"is leap year {is_leap_year(2100)}")
+print(2100 % 4)
+print(2100 % 100)
+print(2100 % 400)
