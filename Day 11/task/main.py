d334ad1df76098a109dd1d57fc0149080d3ad3a5
Louis Martin <louimart@gmail.com>
2024-12-05 20:50:38 -0600

    progress on while loop and if loop to address various scenario of the game play

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -24,8 +24,11 @@ def player_final():
 def dealer_hand():
     print(f"Dealer's first card: {dealer[0]}")
 
+def dealer_full_hand():
+    print(f"Dealer's hand: {dealer}, current score: {sum(dealer)}")
+
 def dealer_final():
-    print(f"Your final hand: {dealer}, final score: {sum(dealer)}")
+    print(f"Dealer's final hand: {dealer}, final score: {sum(dealer)}")
 
 # Code
 deal_input = input("Do you want to play a game of BlackJack? Y or N\n")
@@ -36,20 +39,25 @@ if deal_input.lower() == "y":
     while sum(player) <= 21:
         if sum(player) == 21:
             print("you WIN")
-            break
+            while sum(dealer) <= 16:
+                dealer.append(random.choice(cards))
+                dealer_full_hand()
+            dealer_final()
         else:
             hit = input("Type 'Y' to get another card, Type 'N' to pass\n")
             if hit.lower() == "y":
                 player.append(random.choice(cards))
                 player_hand()
                 dealer_hand()
-            else:
-                break
+            elif hit.lower() == "n":
+                dealer_hand()
     else:
+        print("you went over")
+        while sum(dealer) <= 16:
+            dealer.append(random.choice(cards))
+            dealer_full_hand()
         player_final()
         dealer_final()
-
-        print("you went over")
 
 
 
